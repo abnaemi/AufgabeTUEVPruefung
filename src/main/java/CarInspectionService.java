0e279a264cf18b85e0b5b679fcc41d13df44cad1
abnae <ab.naemi@gmail.com>
2023-03-07 17:27:37 +0100

    Added the checkCar Method to check all Methods.

--- src/main/java/CarInspectionService.java
+++ src/main/java/CarInspectionService.java
@@ -1,2 +1,40 @@
 public class CarInspectionService {
+
+
+  public boolean fourTiresChecker(Car car){
+         if (car.getNumberOfTires() == 4) {
+             return true;
+         }
+         return false;
+  }
+
+    public boolean seatBeltChecker(Car car){
+        if (car.airBagExists() == true) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean airBagChecker(Car car){
+      if (car.airBagExists()==true) {
+          return true;
+      }
+       return false;
+    }
+
+    public boolean doorNumbersCorrectChecker (Car car){
+       if ((car.getNumberOfDoors() == 3) || (car.getNumberOfDoors() == 3)){
+           return true;
+        }  return false;
+    }
+
+    public boolean checkCar (Car car){
+      if ((car.getNumberOfTires() == 4) && (car.airBagExists() == true) &&  (car.airBagExists()==true) && ((car.getNumberOfDoors() == 3) || (car.getNumberOfDoors() == 3)){
+          return true;
+        }
+        return false;
+    }
+
+
+
 }
