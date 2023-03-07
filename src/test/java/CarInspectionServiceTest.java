6cfc9e1313d201813daba7ae28148e9f808b073e
abnae <ab.naemi@gmail.com>
2023-03-07 17:49:40 +0100

    Added fourTiresChecker Tests.

--- src/test/java/CarInspectionServiceTest.java
+++ src/test/java/CarInspectionServiceTest.java
@@ -1,10 +1,32 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarInspectionServiceTest {
 
     @org.junit.jupiter.api.Test
-    void fourTiresChecker() {
+    void fourTiresCheckerTrue() {
+
+        Car carwithfourtires = new Car ();
+        carwithfourtires.setNumberofTires(4);
+        Car actualcar = new Car ();
+        actualcar.setNumberofTires(4);
+
+        assertEquals(carwithfourtires.getNumberOfTires(), actualcar.getNumberOfTires());
     }
+
+
+    @org.junit.jupiter.api.Test
+    void fourTiresCheckerFalse() {
+
+        Car carwithfourtires = new Car();
+        carwithfourtires.setNumberofTires(4);
+        Car actualcar = new Car();
+        actualcar.setNumberofTires(3);
+
+        assertNotEquals(carwithfourtires.getNumberOfTires(), actualcar.getNumberOfTires());
+    }
+
 
     @org.junit.jupiter.api.Test
     void seatBeltChecker() {
