9ceae51b32a058a5b0898483ef21cb1b3439e321
abnae <ab.naemi@gmail.com>
2023-03-07 19:37:46 +0100

    Added full tests for seatbelt Method.

--- src/test/java/CarInspectionServiceTest.java
+++ src/test/java/CarInspectionServiceTest.java
@@ -12,16 +12,12 @@ class CarInspectionServiceTest {
         Car carExpected= new Car (4,4,true,true);
         CarInspectionService tüv = new CarInspectionService();
         carExpected.setNumberOfTires(2);
-        System.out.println(carExpected.getNumberOfTires());
-        System.out.println(tüv.fourTiresChecker(carExpected));
-        System.out.println(tüv.seatBeltChecker(carExpected));
+
 
         Car carActual= new Car (4,4,true,true);
         CarInspectionService tüv2 = new CarInspectionService();
         carActual.setNumberOfTires(2);
-        System.out.println(carActual.getNumberOfTires());
-        System.out.println(tüv2.fourTiresChecker(carActual));
-        System.out.println(tüv2.seatBeltChecker(carActual));
+
 
         assertEquals(tüv.fourTiresChecker(carExpected),tüv2.fourTiresChecker(carActual));
 
