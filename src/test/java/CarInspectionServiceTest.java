ab1496b0191c37959f633faf0f01d607eb1ec5ed
abnae <ab.naemi@gmail.com>
2023-03-07 19:28:06 +0100

    Fully added tests for the first Method.

--- src/test/java/CarInspectionServiceTest.java
+++ src/test/java/CarInspectionServiceTest.java
@@ -26,6 +26,25 @@ class CarInspectionServiceTest {
         assertEquals(tüv.fourTiresChecker(carExpected),tüv2.fourTiresChecker(carActual));
 
     }
+    @Test
+    void carHas4TiresFalse() {
 
+        Car carExpected= new Car (4,4,true,true);
+        CarInspectionService tüv = new CarInspectionService();
+        carExpected.setNumberOfTires(2);
+        System.out.println(carExpected.getNumberOfTires());
+        System.out.println(tüv.fourTiresChecker(carExpected));
+        System.out.println(tüv.seatBeltChecker(carExpected));
+
+        Car carActual= new Car (1,4,true,true);
+        CarInspectionService tüv2 = new CarInspectionService();
+        carActual.setNumberOfTires(2);
+        System.out.println(carActual.getNumberOfTires());
+        System.out.println(tüv2.fourTiresChecker(carActual));
+        System.out.println(tüv2.seatBeltChecker(carActual));
+
+        assertNotEquals(tüv.fourTiresChecker(carExpected),tüv2.fourTiresChecker(carActual));
+
+    }
 
 }
