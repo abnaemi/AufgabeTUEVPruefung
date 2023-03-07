2214f90a07c9b248448a4ff1f3c4a9125a17c746
abnae <ab.naemi@gmail.com>
2023-03-07 19:37:06 +0100

    Added full tests for seatbelt Method.

--- src/test/java/CarInspectionServiceTest.java
+++ src/test/java/CarInspectionServiceTest.java
@@ -32,19 +32,41 @@ class CarInspectionServiceTest {
         Car carExpected= new Car (4,4,true,true);
         CarInspectionService tüv = new CarInspectionService();
         carExpected.setNumberOfTires(2);
-        System.out.println(carExpected.getNumberOfTires());
-        System.out.println(tüv.fourTiresChecker(carExpected));
-        System.out.println(tüv.seatBeltChecker(carExpected));
+
 
         Car carActual= new Car (1,4,true,true);
         CarInspectionService tüv2 = new CarInspectionService();
         carActual.setNumberOfTires(2);
-        System.out.println(carActual.getNumberOfTires());
-        System.out.println(tüv2.fourTiresChecker(carActual));
-        System.out.println(tüv2.seatBeltChecker(carActual));
+
 
         assertNotEquals(tüv.fourTiresChecker(carExpected),tüv2.fourTiresChecker(carActual));
 
     }
 
+    @Test
+    void seatBeltCheckerTrue() {
+
+        Car carOne= new Car (4,4,true,true);
+        CarInspectionService tüv = new CarInspectionService();
+
+        Car carTwo= new Car (4,4,true,true);
+        CarInspectionService tüvTwo = new CarInspectionService();
+
+        assertEquals(carOne,carTwo);
+
+
+    }
+    @Test
+    void seatBeltCheckerFalse() {
+
+        Car carOne= new Car (4,4,true,true);
+        CarInspectionService tüv = new CarInspectionService();
+
+        Car carTwo= new Car (4,4,false,true);
+        CarInspectionService tüvTwo = new CarInspectionService();
+
+        assertNotEquals(carOne,carTwo);
+
+
+    }
 }
